44e8f0dbe4e5c03286cfaf2cc15c19a0d1cbdcae
Dima0913 <dimarovno2009@gmail.com>
2024-01-22 22:15:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,101 @@
+# EX1
+# start = int(input("enter your number1: "))
+# end = int(input("enter your number2: "))
+# sum1 = 0
+# sum2 = 0
+# sum3 = 0
+# count1 = 0
+# count2 = 0
+# count3 = 0
+
+# for i in range(start,end):
+#     if i%2 == 0:
+#         sum1 += i
+#         count1 += 1
+#     if i%2 != 0:
+#         sum2 += i
+#         count2 += 1
+#     if i%9 == 0:
+#         sum3 += i
+#         count3 += 1
+# avarage1 = 0
+# avarage2 = 0
+# avarage3 = 0
+# if count1 != 0:
+#     avarage1 = sum1/count1
+# if count2 != 0:
+#     avarage2 = sum2/count2  
+# if count3 != 0:
+#     avarage3 = sum3/count3
+# print(f"avarage of first group is: {avarage1} ")
+# print(f"avarage of second group is: {avarage2} ")
+# print(f"avarage of third group is: {avarage3} ")
+
+# ex2
+# length = int(input("enter length of line: "))
+# sumbol = input("enter sumbol: ")
+# for i in range(0,length):
+#     print(sumbol)
+    
+# ex3
+# num = int(input("enter nums: "))
+# while num != 7:
+#     if num > 0:
+#         print("Num is positive")
+#     elif num < 0:
+#         print("Num is negative")
+#     elif num == 0:
+#         print("Number is equal to zero")
+#     num = int(input("enter nums: "))
+# print("Good bye")       
+
+
+
+# ex4
+num = int(input("enter num: "))
+sum = 0
+min = num
+max = num
+while num !=7:
+    sum+=num
+    if num < min:
+        min = num
+    if num > max:
+        max = num
+    num = int(input("enter num: "))
+print(f"suma = {sum}")
+print(f"min = {min}")
+print(f"max = {max}")
+print("Good bye!") 
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
